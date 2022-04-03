33b4135e6c99db1cbbea4bd39c51a6c02b453ef5
Alan Hamid <alanhamid13@gmail.com>
2022-04-03 18:22:42 -0400

    Metadat uploaded

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Camel Jockey Club NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Camel Jockey Club is a multilingual metaverse, a member exclusive private community, where like-minded people all across the world can come together and talk about their bets and investments in their native languages to anyone in the community.  Camel Jockey Club will be powered by Blu:  A proprietary tech of Texila, Inc.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
