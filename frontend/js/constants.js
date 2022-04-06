2144271d3d3f5e26b4eab5dafe5ca9e7a62b4165
Alan Hamid <alanhamid13@gmail.com>
2022-04-06 18:00:29 -0400

    updated constandt.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "0xa733D2a04BF090F8E89f4F323b7944DF4C58Df34"; //Replace with your own contract address
+const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Own the Camel Jockey Club NFTs Become the Owners of the Billion+ Company in the Making";
-const welcome_p = 'Each CJC NFT gives you 0.00001 Stock Option in Texila, Inc.  Currently valued at 15M.  <br>To learn more, please visit <a href = "https://www.texila.io>" wwww.texila.io</a>';
-
+const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h2 = "Connect to MetaMask to Get Started";
+const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
